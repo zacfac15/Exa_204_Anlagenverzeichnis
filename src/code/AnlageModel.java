e7dc20f569b4e0444273f31fcc466a7033bb46ff
Fabian Zach <zacfac15@new.edvhtl.at>
2018-10-09 10:11:11 +0200

    Edited Model

--- src/code/AnlageModel.java
+++ src/code/AnlageModel.java
@@ -1,69 +1,95 @@
 package code;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
-public class AnlageModel extends AbstractTableModel {
+public class AnlageModel extends AbstractTableModel
+{
 
-    private ArrayList<Anlage> anlagen = new ArrayList<>();
-    private int year;
+  private ArrayList<Anlage> anlagen = new ArrayList<>();
+  private String filename = System.getProperty("user.dir")
+          + File.separator + "src"
+          + File.separator + "res"
+          + File.separator + "anlagenverzeichnis.csv";
+  private int year;
 
-    private static final String[] colNames = {"Bezeichnung", "AK", "Inbetriebnahme", "ND", "bish. ND", "AfA bisher", "Wert vor Afs", "AfA d. J.", "BW 31.12"};
+  private static final String[] colNames =
+  {
+    "Bezeichnung", "AK", "Inbetriebnahme", "ND", "bish. ND", "AfA bisher", "Wert vor Afs", "AfA d. J.", "BW 31.12"
+  };
 
+  @Override
+  public String getColumnName(int i)
+  {
+    return colNames[i];
+  }
 
-    public void addAnlage(Anlage a) {
-        anlagen.add(a);
+  @Override
+  public int getRowCount()
+  {
+    return anlagen.size();
+  }
+
+  @Override
+  public int getColumnCount()
+  {
+    return colNames.length;
+  }
+
+  @Override
+  public Object getValueAt(int rowIndex, int columnIndex)
+  {
+    Anlage a = anlagen.get(rowIndex);
+    return a;
+  }
+
+  public void addAnlage(Anlage a)
+  {
+    anlagen.add(a);
+  }
+
+  public void saveFile() throws Exception
+  {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename)))
+    {
+      bw.write("");
+      for (Anlage a : anlagen)
+      {
+        bw.append(a.toCSV());
+      }
     }
-
-    public void saveFile() throws Exception {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("anlagenverzeichnis.csv"))) {
-            bw.write("");
-            for (Anlage a : anlagen) {
-                bw.append(a.toCSV());
-            }
-        } catch (IOException e) {
-            throw new Exception("Reading file failed: " + e.getMessage());
-        }
+    catch (IOException e)
+    {
+      throw new Exception("Reading file failed: " + e.getMessage());
     }
+  }
 
-    public void loadFile() throws Exception {
-        try (BufferedReader bw = new BufferedReader(new FileReader("anlagenverzeichnis.csv"))) {
-            String line;
+  public void loadFile() throws Exception
+  {
+    try (BufferedReader bw = new BufferedReader(new FileReader(filename)))
+    {
+      String line;
 
-            while ((line = bw.readLine()) != null) {
-                addAnlage(new Anlage(line));
-            }
-        } catch (IOException e) {
-            throw new Exception("Loading file failed: " + e.getMessage());
-        }
+      while ((line = bw.readLine()) != null)
+      {
+        addAnlage(new Anlage(line));
+      }
     }
-
-    public void updateTable(int year) {
-        
+    catch (IOException e)
+    {
+      throw new Exception("Loading file failed: " + e.getMessage());
     }
+  }
 
-    @Override
-    public String getColumnName(int i) {
-        return colNames[i];
-    }
+  public void updateTable(int year)
+  {
 
-    @Override
-    public int getRowCount() {
-        return anlagen.size();
-    }
+  }
 
-    @Override
-    public int getColumnCount() {
-        return colNames.length;
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        Anlage a = anlagen.get(rowIndex);
-        return a;
-    }
 }
