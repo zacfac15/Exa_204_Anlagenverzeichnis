c67e8f58b16676d3e81e3ba4ccf4f79933041f2e
marcx28 <35005352+marcx28@users.noreply.github.com>
2018-10-09 09:23:55 +0200

    Add Anlage, loading and saving of csv file

---
+++ src/code/Anlage.java
@@ -0,0 +1,38 @@
+package code;
+
+public class Anlage {
+
+    private String bezeichnug;
+    private int ak;
+    private int inbetriebnahme;
+    private int nd;
+
+    public Anlage(String line) {
+        String[] parts = line.split(",");
+        bezeichnug = parts[0];
+        ak = Integer.parseInt(parts[1]);
+        inbetriebnahme = Integer.parseInt(parts[2]);
+        nd = Integer.parseInt(parts[3]);
+    }
+
+    public String getBezeichnug() {
+        return bezeichnug;
+    }
+
+    public int getAk() {
+        return ak;
+    }
+
+    public int getInbetriebnahme() {
+        return inbetriebnahme;
+    }
+
+    public int getNd() {
+        return nd;
+    }
+
+    public String toCSV() {
+        return String.format("%s,%d,%d,%d", bezeichnug, ak, inbetriebnahme, nd);
+    }
+
+}
---
+++ src/code/AnlageBL.java
@@ -0,0 +1,45 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AnlageBL {
+
+    private ArrayList<Anlage> anlagen = new ArrayList<>();
+    private int year;
+
+    public void addAnlage(Anlage a) {
+        anlagen.add(a);
+    }
+
+    public void saveFile() throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("anlagenverzeichnis.csv"))) {
+            bw.write("");
+            for (Anlage a : anlagen) {
+                bw.append(a.toCSV());
+            }
+        } catch (IOException e) {
+            throw new Exception("Reading file failed: " + e.getMessage());
+        }
+    }
+
+    public void loadFile() throws Exception {
+        try (BufferedReader bw = new BufferedReader(new FileReader("anlagenverzeichnis.csv"))) {
+            String line;
+
+            while ((line = bw.readLine()) != null) {
+                addAnlage(new Anlage(line));
+            }
+        } catch (IOException e) {
+            throw new Exception("Loading file failed: " + e.getMessage());
+        }
+    }
+
+    public void updateTable(int year) {
+
+    }
+}
