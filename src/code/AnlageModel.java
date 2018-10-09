6d99b97d8854b92c6ace304a55ac18246c629a2f
marcx28 <35005352+marcx28@users.noreply.github.com>
2018-10-09 09:59:52 +0200

    add TableCellRenderer

--- src/code/AnlageModel.java
+++ src/code/AnlageModel.java
@@ -6,53 +6,64 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
 
-public class AnlageModel
-{
+public class AnlageModel extends AbstractTableModel {
 
-  private ArrayList<Anlage> anlagen = new ArrayList<>();
-  private int year;
+    private ArrayList<Anlage> anlagen = new ArrayList<>();
+    private int year;
 
-  public void addAnlage(Anlage a)
-  {
-    anlagen.add(a);
-  }
+    private static final String[] colNames = {"Bezeichnung", "AK", "Inbetriebnahme", "ND", "bish. ND", "AfA bisher", "Wert vor Afs", "AfA d. J.", "BW 31.12"};
 
-  public void saveFile() throws Exception
-  {
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter("anlagenverzeichnis.csv")))
-    {
-      bw.write("");
-      for (Anlage a : anlagen)
-      {
-        bw.append(a.toCSV());
-      }
+    public void addAnlage(Anlage a) {
+        anlagen.add(a);
     }
-    catch (IOException e)
-    {
-      throw new Exception("Reading file failed: " + e.getMessage());
+
+    public void saveFile() throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("anlagenverzeichnis.csv"))) {
+            bw.write("");
+            for (Anlage a : anlagen) {
+                bw.append(a.toCSV());
+            }
+        } catch (IOException e) {
+            throw new Exception("Reading file failed: " + e.getMessage());
+        }
     }
-  }
 
-  public void loadFile() throws Exception
-  {
-    try (BufferedReader bw = new BufferedReader(new FileReader("anlagenverzeichnis.csv")))
-    {
-      String line;
+    public void loadFile() throws Exception {
+        try (BufferedReader bw = new BufferedReader(new FileReader("anlagenverzeichnis.csv"))) {
+            String line;
 
-      while ((line = bw.readLine()) != null)
-      {
-        addAnlage(new Anlage(line));
-      }
+            while ((line = bw.readLine()) != null) {
+                addAnlage(new Anlage(line));
+            }
+        } catch (IOException e) {
+            throw new Exception("Loading file failed: " + e.getMessage());
+        }
     }
-    catch (IOException e)
-    {
-      throw new Exception("Loading file failed: " + e.getMessage());
+
+    public void updateTable(int year) {
+        
     }
-  }
 
-  public void updateTable(int year)
-  {
+    @Override
+    public String getColumnName(int i) {
+        return colNames[i];
+    }
 
-  }
+    @Override
+    public int getRowCount() {
+        return anlagen.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Anlage a = anlagen.get(rowIndex);
+        return a;
+    }
 }
